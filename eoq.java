8716977cb9705b834fd058c09ca17d80a7eb5d33
sandy334 <hrsandesh3@gmail.com>
2025-07-02 11:03:33 +0530

    Eoq

---
+++ eoq.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class eoq {
+    public static void main(String[] args) {  
+        Scanner sc = new Scanner(System.in);
+        double a_d_m = sc.nextDouble();
+        double c_m_p = sc.nextDouble();
+        double c_s_p = sc.nextDouble();
+        double eoq = Math.sqrt(2 * a_d_m * c_m_p / c_s_p);
+        System.out.println(eoq);
+        // EOQ with 10% increase in cost per unit and 10% decrease in storing cost
+        double eoq_both = Math.sqrt(2 * a_d_m * (c_m_p * 1.1) / (c_s_p * 0.9));
+        System.out.println(eoq_both);
+        // Print the difference between the two EOQ values
+        System.out.println(eoq_both - eoq);
+        // Print the percentage difference
+        double percent_diff = ((eoq_both - eoq) / eoq) * 100;
+        System.out.println(percent_diff);
+        sc.close();
+    }
+}
