c7feea033693670c3e0cec1ba558cb67ca3a8918
sandy334 <hrsandesh3@gmail.com>
2025-07-02 14:29:53 +0530

    updated

--- delay.java
+++ delay.java
@@ -1,12 +1,10 @@
 // Scenario: Mr kashyap is working as software engineer finalizing spirints(short meeting) for the banking projects, the patchup work of software has a timing deadline of 5 seconds and an alarm is setup using a counter to display the reverse counting of the timer generate a code help mr kashyap.
 public class delay {
     public static void main(String[] args) throws InterruptedException {
-        int seconds = 5; // Set the countdown time in seconds
-        while (seconds >= 0) {
-            System.out.println(seconds);
+        int i = 5; // Starting from 5 seconds
+        for(int sec= i; sec >= 0; sec--) {
+            System.out.println("Time left: " + sec + " seconds");
             Thread.sleep(1000); // Wait for 1 second
-            seconds--; // Decrement the counter
-        }
-     
+        }   
     }
 }
