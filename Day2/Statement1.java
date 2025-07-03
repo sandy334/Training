3971e67da582d165117fc52ac41c951aca47f7eb
sandy334 <hrsandesh3@gmail.com>
2025-07-03 09:17:39 +0530

    made a proper folder of training day wise

--- Day2/Statement1.java
+++ Day2/Statement1.java
@@ -1,4 +1,5 @@
 //Scenario:Mr vimal is working as a plumber repairing tanks of spherical nature in an appartment.In an appartment he came across a tank that had a small slit through which water leaks as a result the tank is half empty.After fixing the issue he wants to access the macimum capacity of the tank.Guide him to write a code with a radius of 21cm for a tank as such.
+//formula = (4/3)*pi*(radius*radius*radius)
 
 public class Statement1 {
     public static void main(String[]args){
