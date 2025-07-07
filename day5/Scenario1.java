2155bf49e1251f6f0490d97970a36f0bb9166ff6
sandy334 <hrsandesh3@gmail.com>
2025-07-07 10:28:40 +0530

    updated

---
+++ day5/Scenario1.java
@@ -0,0 +1,26 @@
+//MR amal is a pharmacist who has a particular type of medicinal combinations that are measured in milli grams that may be classified into two types , type 1 : {0.23,0.44,0.74,0.83,0.37} type 2 : {4.67,4.83,3.14,4.24,5.31} ie addition of first type to second type will result in a new type of medicinal combination that is equal to the sum of the two types.
+
+public class Scenario1 {
+    public static void main(String[] args) {
+        double[] type1 = {0.23, 0.44, 0.74, 0.83, 0.37};
+        double[] type2 = {4.67, 4.83, 3.14, 4.24, 5.31};
+
+        System.out.println("Type 1 Medicinal Combinations:");
+        for (double value : type1) {
+            System.out.print(value + " ");
+        }
+        System.out.println();
+
+        System.out.println("Type 2 Medicinal Combinations:");
+        for (double value : type2) {
+            System.out.print(value + " ");
+        }
+        System.out.println();
+
+        System.out.println("Resulting Medicinal Combinations:");
+        for (int i = 0; i < type1.length; i++) {
+            double result = type1[i] + type2[i];
+            System.out.print(result + " ");
+        }
+    }
+}
