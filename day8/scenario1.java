8b947f2e9e7963b2906258f1b7582d0d9e59ae36
sandy334 <hrsandesh3@gmail.com>
2025-07-10 10:01:47 +0530

    changes

--- day8/scenario1.java
+++ day8/scenario1.java
@@ -6,26 +6,18 @@ public class scenario1 {
     public static void main(String[] args) {
         Queue<String> messageQueue = new LinkedList<>();
         Scanner scanner = new Scanner(System.in);
-        
-        // Simulate encryption and decryption process
         while (true) {
             System.out.println("Enter a message to encrypt (or type 'exit' to quit): ");
             String input = scanner.nextLine();
-            
             if (input.equalsIgnoreCase("exit")) {
                 break;
             }
-            
-            // Encrypt the message (simple reversal for demonstration)
             String encryptedMessage = new StringBuilder(input).reverse().toString();
             messageQueue.add(encryptedMessage);
             System.out.println("Encrypted message added to queue: " + encryptedMessage);
-            
-            // Decrypt the message
             String decryptedMessage = new StringBuilder(encryptedMessage).reverse().toString();
             System.out.println("Decrypted message: " + decryptedMessage);
         }
-        
         scanner.close();
     }
 }
