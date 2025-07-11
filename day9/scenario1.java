23dd4454ca0d0af3e58c8964be34f4c98a80e084
sandy334 <hrsandesh3@gmail.com>
2025-07-11 13:46:51 +0530

    scenario

---
+++ day9/scenario1.java
@@ -0,0 +1,41 @@
+//Perform  a selection sort for a movie rating app highlighiting the rating of the users based on the movie title  
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.List;
+import java.util.stream.Collectors;
+public class scenario1 {
+    public static void main(String[] args) {
+        List<Movie> movies = Arrays.asList(
+            new Movie("Inception", 8.8),
+            new Movie("The Dark Knight", 9.0),
+            new Movie("Interstellar", 8.6),
+            new Movie("Pulp Fiction", 8.9)
+        );
+
+        List<Movie> sortedMovies = movies.stream()
+            .sorted(Comparator.comparingDouble(Movie::getRating).reversed())
+            .collect(Collectors.toList());
+
+        sortedMovies.forEach(movie -> 
+            System.out.println(movie.getTitle() + ": " + movie.getRating())
+        );
+    }
+}
+
+class Movie {
+    private String title;
+    private double rating;
+
+    public Movie(String title, double rating) {
+        this.title = title;
+        this.rating = rating;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public double getRating() {
+        return rating;
+    }
+}
